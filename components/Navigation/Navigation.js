04288926ab081490e67d46d225d3d70fcc34619f
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-01 17:32:54 +0100

    created header with navigation links

---
+++ components/Navigation/Navigation.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const Header = styled.header`
+  height: 60px;
+  background-color: #222;
+  width: 100vw;
+  display: flex;
+  margin: 0;
+  padding: 0;
+  align-items: center;
+  justify-content: space-evenly;
+  & a {
+    color: #f0f0f0;
+    text-decoration: none;
+  }
+`;
+
+const Navigation = () => {
+  return (
+    <Header>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/pokemons">All Pokemons</Link>
+        <Link href="/pokemons-ssg">Pokemons SSG</Link>
+        <Link href="/pokemons-ssr">Pokemons SSR</Link>
+        <Link href="/pokemons-isr">POkemons ISR</Link>
+      </nav>
+    </Header>
+  );
+};
+
+export default Navigation;
