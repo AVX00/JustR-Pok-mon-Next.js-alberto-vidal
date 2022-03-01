ae44c4c778efb9ba475d8f574ecb39d170176c29
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-01 17:52:19 +0100

    created all pages for nav links

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <></>;
-}
+const Home = () => {
+  return <h1>home</h1>;
+};
+export default Home;
---
+++ pages/pokemons-isr.js
@@ -0,0 +1,4 @@
+const PokemonISR = () => {
+  return <h1>pokemon-isr</h1>;
+};
+export default PokemonISR;
