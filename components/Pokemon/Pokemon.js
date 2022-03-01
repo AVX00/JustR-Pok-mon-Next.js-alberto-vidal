0cce987cb499e1e710107cf46a67623a21cd6226
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-03-01 20:26:00 +0100

    created a pokemon list at pokemon with csr

--- components/Pokemon/Pokemon.js
+++ components/Pokemon/Pokemon.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Image from "next/image";
 
 const Card = styled.li`
+  position: relative;
   height: 400px;
   width: 280px;
   border-radius: 5px;
@@ -9,19 +10,40 @@ const Card = styled.li`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  background-color: #222;
+  margin: 20px;
   & img {
-    object-position: center;
-    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+  & .stats {
+    height: 50%;
+  }
+  & .image {
+    width: 200px;
+    height: 200px;
+    position: relative;
   }
 `;
 
 const Pokemon = ({ pokemon }) => {
   return (
     <Card>
-      <Image src={pokemon.sprites.front_default} alt={pokemon.name} />
-      <div>
+      <div className="image">
+        <Image
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}
+          layout="fill"
+        />
+      </div>
+      <div className="stats">
         <h2>{pokemon.name}</h2>
       </div>
     </Card>
   );
 };
+
+export default Pokemon;
